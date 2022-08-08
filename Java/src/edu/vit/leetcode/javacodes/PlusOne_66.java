8ad23f4e8e6922c9198dd6ef996d6d968ceb9a29
iamsuteerth <suteerth@gmail.com>
2022-08-08 18:06:47 +0530

    Normal Commit

--- Java/src/edu/vit/leetcode/javacodes/PlusOne_66.java
+++ Java/src/edu/vit/leetcode/javacodes/PlusOne_66.java
@@ -1,36 +1,30 @@
 package edu.vit.leetcode.javacodes;
 
-import java.util.Arrays;
+class Solution {
+    public int[] plusOne(int[] digits) {
+        // We will implement a simple carry over system
+        int carryOver = 1; // Default carry over is 1
+        int sum = 0; // Initializing sum
+        for (int i = digits.length - 1; i >= 0; i--) { // Starting from end
+            sum = digits[i] + carryOver; // this can be either value + 1 (which we want) or it will be a value greater
+                                         // than 10
+            if (sum >= 10) {
+                carryOver = 1;
+            } else {
+                carryOver = 0;
+            }
+            digits[i] = sum % 10; // If we are having 9, 9%10 is 9 only, so we retain the value, in case it's 12
+                                  // or 11, then it's 1 or 2
+        }
+        if (carryOver == 1) { // This means that we need to increase array size by one
+            int[] plusOneArray = new int[digits.length + 1];
+            System.arraycopy(digits, 0, plusOneArray, 1, digits.length); // Source, Source Position, Destination,
+                                                                         // Destination Position, length
+            plusOneArray[0] = 1; // Best way to understand this statement is by assuming input array to be 9
+            return plusOneArray;
+        } else {
+            return digits;
+        }
 
-public class PlusOne_66 {
-    public static long converter(int[] digits) {
-        long ans = 0;
-        for(int i = 0 ; i < digits.length ; i++){
-            ans = ans*10 + (long)digits[i];
-        }
-        System.out.println(ans);
-        return ans;
-    }
-    public static int sizer(long nums){
-        String temp = Long.toString(nums);
-        int size = temp.length();
-        return size;
-    }
-    public static int[] plusOne(int[] digits) {
-        long num = converter(digits);
-        num++;
-        int size = sizer(num);
-        System.out.println(size);
-        String arr = Long.toString(num); 
-        int[] array = new int[size];
-        for(int i = 0 ; i < size; i++){
-            array[i] = (int)(arr.charAt(i)-'0');
-        }
-        return array;
-    }
-    public static void main(String[] args) {
-        int[] digits = new int[]{9,8,7,6,5,4,3,2,1,0};
-        int[] ans = plusOne(digits);
-        System.out.println(Arrays.toString(ans));
     }
 }
---
+++ Java/src/edu/vit/leetcode/javacodes/PlusOne_66.md
@@ -0,0 +1,27 @@
+# Plus One
+## Problem Description
+<font size = "3">
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+</font>
+
+## Sample Inputs/Outputs
+
+***1*** 
+
+*Input*: **Input_Array** = [4,3,2,1]
+
+*Output*: **Output_Array** = [4,3,2,2]
+
+***2*** 
+
+*Input*: **Input_Array** = [1,2,3]
+
+*Output*: **Output_Array** = [1,2,4]
+
+***3*** 
+
+*Input*: **Input_Array** = [9]
+
+*Output*: **Output_Array** = [1,0]
