1198c3c2d231e5da7035f2abf28b7eaf6b22c590
iamsuteerth <suteerth@gmail.com>
2023-03-10 23:49:51 +0530

    Daily Problem Solved 100323

---
+++ C++/0382_LLRandomNode.cpp
@@ -0,0 +1,41 @@
+/* 
+ * Name : Suteerth Subramaniam
+ * Language Used : C++
+ * Question Link : https://leetcode.com/problems/linked-list-random-node/
+ */
+#include <random>
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* temp;
+    Solution(ListNode* head) {
+        temp = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        int res;
+        ListNode* curr = temp;
+        int size = 1;
+        while(curr) {
+            //choosing probability is 1/size (reservior sampling)
+            if(rand()%size == 0)
+                res = curr->val;
+            curr = curr->next;
+            size++;
+        }
+        return res;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
