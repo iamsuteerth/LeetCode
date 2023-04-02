949cc24e5ad17b1559271a4fe6ad7c6e06bd40b4
iamsuteerth <suteerth@gmail.com>
2023-04-02 23:36:20 +0530

    Daily Problem Solved 010423 - Easy

---
+++ C++/0704_BinarySearch.cpp
@@ -0,0 +1,28 @@
+/* 
+ * Name : Suteerth Subramaniam
+ * Language Used : C++
+ * Question Link : https://leetcode.com/problems/binary-search/
+ */
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid = 0;
+        while(left <= right){
+            mid = (left + right)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
