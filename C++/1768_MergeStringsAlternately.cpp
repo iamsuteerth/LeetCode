342ba034f59a25f4abb2a4eb8f07b52703e7f811
Suteerth Subramaniam <98741486+iamsuteerth@users.noreply.github.com>
2023-04-18 23:56:14 +0530

    Daily Problem Solved 180423 - Easy

---
+++ C++/1768_MergeStringsAlternately.cpp
@@ -0,0 +1,29 @@
+/* 
+ * Name : Suteerth Subramaniam
+ * Language Used : C++
+ * Question Link : https://leetcode.com/problems/merge-strings-alternately/
+ */
+#include <string>
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int l1 = word1.length();
+        int l2 = word2.length();
+        string res = "";
+        int i = 0;
+        int min = (l1 < l2) ? l1 : l2;
+        while(i < min){
+            res = res + word1[i] + word2[i];
+            i++;
+        }
+        if(l1 < l2){
+            while(i < l2)
+                res += word2[i++];
+        }
+        else{
+            while(i < l1)
+                res += word1[i++];
+        }
+        return res;
+    }
+};
