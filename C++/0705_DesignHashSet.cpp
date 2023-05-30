2bfb06dd799b1a200bdee4fa65965bb9686bccb0
Suteerth Subramaniam <98741486+iamsuteerth@users.noreply.github.com>
2023-05-31 02:21:55 +0530

    Daily Problem Solved 310523 - Easy

---
+++ C++/0705_DesignHashSet.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define prime 104729
+// Largest prime number within the given range which is used for hashing
+class MyHashSet {
+public:
+    vector<int> map;
+    MyHashSet() {
+        map.resize(prime, -1);
+    }
+    // Modulo Division method is used here
+    void add(int key) {
+        int index = key % prime;
+        if (map[index] == -1) {
+            map[index] = key;
+        }
+    }
+
+    void remove(int key) {
+        int index = key % prime;
+        if (map[index] == key) {
+            map[index] = -1;
+        }
+    }
+
+    bool contains(int key) {
+        int index = key % prime;
+        return (map[index] == key);
+    }
+};
