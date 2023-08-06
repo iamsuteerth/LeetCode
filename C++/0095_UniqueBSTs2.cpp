52d35ac176ed232ccdc1cec6d0f5f14a4fada8c8
Suteerth Subramaniam <98741486+iamsuteerth@users.noreply.github.com>
2023-08-06 23:56:03 +0530

    Daily Problem Solved 060823 - Medium

---
+++ C++/0095_UniqueBSTs2.cpp
@@ -0,0 +1,36 @@
+/* 
+ * Name : Suteerth Subramaniam
+ * Language Used : C++
+ * Question Link : https://leetcode.com/problems/unique-binary-search-trees-ii/
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<TreeNode*> solve(int start, int end){
+        if(start > end)
+            return {NULL};
+        if(start == end){
+            TreeNode* root = new TreeNode(start);
+            return {root};
+        }
+        vector<TreeNode*> res;
+        for(int i = start ; i <= end ; i++){
+            vector<TreeNode*> left = solve(start, i - 1);
+            vector<TreeNode*> right = solve(i + 1, end);   
+            for(TreeNode* leftRoot : left) {
+                for(TreeNode* rightRoot : right) {
+                    TreeNode* root = new TreeNode(i);
+                    root->left  = leftRoot;
+                    root->right = rightRoot;
+                    res.push_back(root);
+                }
+            }
+        }
+         return res;
+    }
+    vector<TreeNode*> generateTrees(int n) {
+        return solve(1,n);
+    }
+};
