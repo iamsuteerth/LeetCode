972e85a44e31cb8a642774fc85e9369796861486
Suteerth Subramaniam <98741486+iamsuteerth@users.noreply.github.com>
2023-08-10 23:52:10 +0530

    Daily Problem Solved 050823 - Medium

--- C++/0095_UniqueBSTs2.cpp
+++ C++/0095_UniqueBSTs2.cpp
@@ -9,16 +9,19 @@ using namespace std;
 class Solution {
 public:
     vector<TreeNode*> solve(int start, int end){
-        if(start > end)
+        if(start > end){
             return {NULL};
+        }
         if(start == end){
             TreeNode* root = new TreeNode(start);
             return {root};
         }
         vector<TreeNode*> res;
         for(int i = start ; i <= end ; i++){
+            
             vector<TreeNode*> left = solve(start, i - 1);
             vector<TreeNode*> right = solve(i + 1, end);   
+            
             for(TreeNode* leftRoot : left) {
                 for(TreeNode* rightRoot : right) {
                     TreeNode* root = new TreeNode(i);
