b4df63276717cc70cdeb795c71aba5ebe4a2a8ee
Suteerth Subramaniam <98741486+iamsuteerth@users.noreply.github.com>
2023-08-16 23:56:50 +0530

    Daily Problem Solved 150823 - Medium

---
+++ C++/0086_PartitionList.cpp
@@ -0,0 +1,32 @@
+/* 
+ * Name : Suteerth Subramaniam
+ * Language Used : C++
+ * Question Link : https://leetcode.com/problems/partition-list/
+ */
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* small  = new ListNode(0);
+        ListNode* large  = new ListNode(0);
+        
+        ListNode* smallP = small;
+        ListNode* largeP = large;
+        
+        while(head) {
+            if(head->val < x) {
+                smallP->next = head;
+                smallP = smallP->next;
+            } else {
+                largeP->next = head;
+                largeP = largeP->next;
+            }
+            
+            head = head->next;
+        }
+        largeP->next = NULL;
+        smallP->next = large->next;
+        return small->next;
+    }
+};
