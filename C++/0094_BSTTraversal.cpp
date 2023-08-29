b64ba9c2607f72e10900e38a6d1885d007999cfd
Suteerth Subramaniam <98741486+iamsuteerth@users.noreply.github.com>
2023-08-30 01:02:26 +0530

    General Problem Solving - 290823 - Easy - P4

---
+++ C++/0094_BSTTraversal.cpp
@@ -0,0 +1,31 @@
+/* 
+ * Name : Suteerth Subramaniam
+ * Language Used : C++
+ * Question Link : https://leetcode.com/problems/binary-tree-inorder-traversal/
+ */
+#include <vector>
+#include <iostream>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int> &res){
+        if(!root)   
+            return;
+        inorder(root->left, res);
+        res.push_back(root->val);
+        inorder(root->right, res);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        inorder(root, res);
+        return res;
+    }
+};
